ef4c9717910d915dd674456f08f828cc9e59278f
joseph-duran <duranjoseph267@gmail.com>
2026-09-08 01:37:12 -0400

    1st

--- 02_variables_and_data_types/02_03_personal_message.py
+++ 02_variables_and_data_types/02_03_personal_message.py
@@ -2,3 +2,8 @@
 Joseph Duran
 Chapter 2
 '''
+frist_name = "Kate"
+last_name = "Reber"
+full_name = f"{frist_name} {last_name}"
+message = f"Hello, {full_name}!"
+print(message)
---
+++ 02_variables_and_data_types/02_06_famous_quote2.py
@@ -0,0 +1,5 @@
+Famous_person = "drake"
+
+message = f"{Famous_person} I am not a robot, I am a human being."
+
+print(message)
