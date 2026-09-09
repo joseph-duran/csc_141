bb2c0f75a6e42c797ecd5ca8623b356f926526af
joseph-duran <duranjoseph267@gmail.com>
2026-09-09 10:18:10 -0400

    commit

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,23 @@
+'''
+Joseph Duran
+Chapter 2
+'''
+
+name = "Joseph Duran"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+#This code snippet shows how to manipulate the case 
+#of a string in Python. The #`title()` method capitalizes the first 
+#letter of each word, `upper()` converts the entire string to 
+#uppercase, and `lower()` converts it to lowercase.
+
+
+
+print(92 -64)
+message = 'My favorite number is 28'
+print(message)
+#This is a comment that explains the code above. 
+# It prints the result of 92 minus 64, and then it prints a message about my 
+# favorite number.
