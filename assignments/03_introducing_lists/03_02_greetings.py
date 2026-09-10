0a708424dd41051195dce7096df00a95e3bbcf46
joseph-duran <duranjoseph267@gmail.com>
2026-09-10 14:28:48 -0400

    3rd

--- assignments/03_introducing_lists/03_02_greetings.py
+++ assignments/03_introducing_lists/03_02_greetings.py
@@ -5,7 +5,5 @@ Chapter 3
 
 name = [" Alex ", "Joseph! ", " Jose ", ", Michael? "]
 message = [" = Goodmoring, Hello, How are you doing today? ", " = "
-"Goodmoring, Hello, How are you doing today? ", 
-           " = Goodmoring, Hello, How are you doing today? ", " = "
-           "Goodmoring, Hello, How are you doing today? "]
+"Whats up! ", " = , How are you doing today? ", " = ", " How have you been?"]
 print(name[0] +"," + message[0])
---
+++ assignments/03_introducing_lists/03_03_your_own_list.py
@@ -0,0 +1,11 @@
+'''
+Joseph Duran
+Chapter 3
+
+'''
+
+
+names = ["2026 Porsche Taycan", "Rolls-Royce Ghost", "Bentley Continental GT", "Ferrari 296 GTB"]
+message = ["Wow that car is amazing!", "That's a great choice!", "I've always wanted to own one of those.", "I can't believe how cool that looks!"]
+
+print(message[0] + "," + names[0])
